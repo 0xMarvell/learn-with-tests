4810db7d83d4a6de34d3d862d7eaeba7548a8b00
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-15 14:06:33 +0100

    Added new test for sumslice func

--- arrays-and-slices/array-and-slices_test.go
+++ arrays-and-slices/array-and-slices_test.go
@@ -2,7 +2,7 @@ package arraysandslices
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestSumArr(t *testing.T) {
 	testcases := []struct {
 		numbers [5]int
 		sum     int
@@ -14,7 +14,26 @@ func TestSum(t *testing.T) {
 	}
 
 	for _, test := range testcases {
-		s := Sum(test.numbers)
+		s := SumArr(test.numbers)
+		if s != test.sum {
+			t.Errorf("Sum(%d): %d. Expected: %d, Got: %d", test.numbers, s, test.sum, s)
+		}
+	}
+}
+
+func TestSumSlice(t *testing.T) {
+	testcases := []struct {
+		numbers []int
+		sum     int
+	}{
+		{[]int{1, 2, 3, 4}, 10},
+		{[]int{-3, 4, 1, 13, 42, -33}, 24},
+		{[]int{9, 20, 0, 0, 5, -6, -54}, -26},
+		{[]int{1, -9, 71, -6, -3}, 54},
+	}
+
+	for _, test := range testcases {
+		s := SumSlice(test.numbers)
 		if s != test.sum {
 			t.Errorf("Sum(%d): %d. Expected: %d, Got: %d", test.numbers, s, test.sum, s)
 		}
