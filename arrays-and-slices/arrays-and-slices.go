3f04bd6d69071654b913767f0c9427241fee0ee5
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-15 13:58:51 +0100

    refactored code. all tests passing

--- arrays-and-slices/arrays-and-slices.go
+++ arrays-and-slices/arrays-and-slices.go
@@ -1,10 +1,10 @@
 package arraysandslices
 
 func Sum(numbers [5]int) int {
-	// total:=0
-	// for _,v:=range numbers{
-	// 	total+=v
-	// }
-	// return total
-	return 0
+	total := 0
+	for _, v := range numbers {
+		total += v
+	}
+	return total
+
 }
