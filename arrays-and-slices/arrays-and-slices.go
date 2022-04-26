069b155b995c455d22d11db2cbc60047f120088e
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-26 15:30:20 +0100

    completed SumAll func. All tests passing

--- arrays-and-slices/arrays-and-slices.go
+++ arrays-and-slices/arrays-and-slices.go
@@ -19,5 +19,12 @@ func SumSlice(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = SumSlice(numbers)
+	}
 	return
 }
