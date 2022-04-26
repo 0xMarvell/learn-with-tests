267f43195a0a03152bd3998482cfeb417238ee54
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-26 15:59:13 +0100

    added new test. test failing (expected)

--- structs-methods-interfaces/structs-methods-interfaces_test.go
+++ structs-methods-interfaces/structs-methods-interfaces_test.go
@@ -16,3 +16,18 @@ func TestPerimeter(t *testing.T) {
 		}
 	}
 }
+
+func TestArea(t *testing.T) {
+	testcases := []struct{ length, width, area float64 }{
+		{10, 10, 100},
+		{3.5, 6.72, 23.52},
+		{13, 77.95, 1013.35},
+	}
+
+	for _, test := range testcases {
+		a := Area(test.length, test.width)
+		if a != test.area {
+			t.Errorf("Perimeter(%.2f, %.2f): %.2f. Expected: %.2f", test.length, test.width, a, test.area)
+		}
+	}
+}
