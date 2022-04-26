714f2199d1aaf2ecf9d6fbef99ca3dab8f221d50
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-26 16:32:44 +0100

    refactor

--- structs-methods-interfaces/structs-methods-interfaces_test.go
+++ structs-methods-interfaces/structs-methods-interfaces_test.go
@@ -3,16 +3,19 @@ package smi
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	testcases := []struct{ length, width, perimeter float64 }{
-		{10, 10, 40},
-		{0.5, 3.442, 7.884},
-		{12, 444.24, 912.48},
+	testcases := []struct {
+		rectangle Rectangle
+		perimeter float64
+	}{
+		{Rectangle{10, 10}, 40},
+		{Rectangle{0.5, 3.442}, 7.884},
+		{Rectangle{12, 444.24}, 912.48},
 	}
 
 	for _, test := range testcases {
-		p := Perimeter(test.length, test.width)
+		p := Perimeter(test.rectangle)
 		if p != test.perimeter {
-			t.Errorf("Perimeter(%.3f, %.3f): %.3f. Expected: %.3f", test.length, test.width, p, test.perimeter)
+			t.Errorf("Perimeter(%.3f): %.3f. Expected: %.3f", test.rectangle, p, test.perimeter)
 		}
 	}
 }
