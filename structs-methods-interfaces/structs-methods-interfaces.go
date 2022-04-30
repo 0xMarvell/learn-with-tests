2d5d2a44dd41ed07bb62c59788782679b1e81b63
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-04-30 20:37:02 +0100

    tests passing..finally

--- structs-methods-interfaces/structs-methods-interfaces.go
+++ structs-methods-interfaces/structs-methods-interfaces.go
@@ -1,13 +1,22 @@
 package smi
 
+import "math"
+
 type Rectangle struct {
 	length, width float64
+}
+type Circle struct {
+	radius float64
 }
 
 func Perimeter(rec Rectangle) float64 {
 	return 2 * (rec.length + rec.width)
 }
 
-func Area(rec Rectangle) float64 {
+func (rec Rectangle) Area() float64 {
 	return rec.length * rec.width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
--- structs-methods-interfaces/structs-methods-interfaces_test.go
+++ structs-methods-interfaces/structs-methods-interfaces_test.go
@@ -1,6 +1,8 @@
 package smi
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	testcases := []struct {
@@ -21,19 +23,51 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	testcases := []struct {
-		rectangle Rectangle
-		area      float64
-	}{
-		{Rectangle{10, 10}, 100},
-		{Rectangle{3.5, 6.72}, 23.52},
-		{Rectangle{13, 77.95}, 1013.35},
-	}
-
-	for _, test := range testcases {
-		a := Area(test.rectangle)
-		if a != test.area {
-			t.Errorf("Area(%.2f): %.2f. Expected: %.2f", test.rectangle, a, test.area)
+	t.Run("testing rectangles", func(t *testing.T) {
+		testcases := []struct {
+			rectangle Rectangle
+			area      float64
+		}{
+			{Rectangle{10, 10}, 100},
+			{Rectangle{3.5, 6.72}, 23.52},
+			{Rectangle{13, 77.95}, 1013.35},
 		}
-	}
+
+		for _, test := range testcases {
+			a := test.rectangle.Area()
+			if a != test.area {
+				t.Errorf("Area(%.2f): %.2f. Expected: %.2f", test.rectangle, a, test.area)
+			}
+		}
+	})
+
+	// t.Run("testing circles", func(t *testing.T) {
+	// 	testcases := []struct {
+	// 		circle Circle
+	// 		area   float64
+	// 	}{
+	// 		{Circle{10}, 314.159265},
+	// 		{Circle{3.5}, 38.484510},
+	// 		{Circle{13}, 530.929158},
+	// 	}
+
+	// 	for _, test := range testcases {
+	// 		a := test.circle.Area()
+	// 		if a != test.area {
+	// 			fmt.Printf("a=%f, test.area=%f\n", a, test.area)
+	// 			fmt.Println(test.area == a)
+	// 			t.Errorf("Area(%f): %f. Expected: %f", test.circle, a, test.area)
+	// 		}
+	// 	}
+	// })
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
