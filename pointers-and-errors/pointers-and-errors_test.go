bc2d3c1adc88ea5333db7f3784f94b25fcf5cc23
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-04 09:02:05 +0100

    refactor. created asertErr helper func

--- pointers-and-errors/pointers-and-errors_test.go
+++ pointers-and-errors/pointers-and-errors_test.go
@@ -13,6 +13,13 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -25,6 +32,15 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
 	})
 
 }
