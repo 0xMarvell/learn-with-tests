765a7e5b9bf16b560bddf8e76be390d9b7b018c7
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-05 11:23:52 +0100

    refactor. moved helper funcs out of main test func

--- pointers-and-errors/pointers-and-errors_test.go
+++ pointers-and-errors/pointers-and-errors_test.go
@@ -4,25 +4,25 @@ import (
 	"testing"
 )
 
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-	assertError := func(t testing.TB, got, want error) {
-		t.Helper()
-
-		if got == nil {
-			t.Fatal("didn't get an error but wanted one")
-		}
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
