a946eb061364a55ab2724b9779c392e960f68809
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-05-16 09:40:42 +0100

    refactor. created assertStrings func

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,13 +3,16 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dic := map[string]string{
-		"test": "this is jst a test",
-	}
-	got := Search(dic, "test")
+	dic := Dictionary{"test": "this is jst a test"}
+	got := dic.Search("test")
 	want := "this is jst a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
